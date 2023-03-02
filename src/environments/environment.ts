daa7ee97124db1ddbeb80dd566be976766a432e6
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-03-02 14:38:32 -0300

    Adding Enviroment Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
 export const environment = {
-    production: false,
   firebase: {
-    projectId: 'kanbanfire-e9876',
-    appId: '1:1020313761380:web:5d55a56c6d6980b0ab1171',
-    storageBucket: 'kanbanfire-e9876.appspot.com',
-    apiKey: 'AIzaSyBYlcrcjHFj_qiX2qSPRuJCUPaCzCJxz0M',
-    authDomain: 'kanbanfire-e9876.firebaseapp.com',
-    messagingSenderId: '1020313761380',
-  }
+    projectId: 'kanbanfire-cf772',
+    appId: '1:556985327472:web:16cc02f0967b001e0939b5',
+    storageBucket: 'kanbanfire-cf772.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDPjzTupmZ5fBlV2goy4CN4cOmMSB4u2hA',
+    authDomain: 'kanbanfire-cf772.firebaseapp.com',
+    messagingSenderId: '556985327472',
+    measurementId: 'G-M2WN4XW6F8',
+  },
+  production: false
 };
